0e3350a565682783f5ae539f1d27f6a3b8d64bf7
wangchen7 <wangchen7@yiche.com>
2019-06-18 16:11:36 +0800

    重试调整

--- CCommon/CCommon.Common/ErrorRetryHelper.cs
+++ CCommon/CCommon.Common/ErrorRetryHelper.cs
@@ -11,13 +11,13 @@ namespace CCommon.Common
     /// </summary>
     public class ErrorRetryHelper
     {
-        public static ReturnResult Handle(string errorRetryConfig, Func<bool> methed)
+        public static ReturnResult Handle(string errorRetryConfig, Func<bool> methed,TimeUnit timeUnit=TimeUnit.Seconds)
         {
             return Handle(errorRetryConfig, () =>
             {
                 var istrue = methed();
                 return istrue ? ReturnResult.SuccessResult() : ReturnResult.FailResult();
-            });
+            }, timeUnit);
         }
 
         /// <summary>
@@ -26,13 +26,13 @@ namespace CCommon.Common
         /// <param name="errorRetryConfig">重试策略(单位:秒)</param>
         /// <param name="methed"></param>
         /// <returns></returns>
-        public static ReturnResult<T> Handle<T>(string errorRetryConfig, Func<ReturnResult<T>> methed)
+        public static ReturnResult<T> Handle<T>(string errorRetryConfig, Func<ReturnResult<T>> methed, TimeUnit timeUnit = TimeUnit.Seconds)
         {
             return Handle(errorRetryConfig, () =>
             {
                 var istrue = methed();
                 return istrue as ReturnResult;
-            }) as ReturnResult<T>;
+            }, timeUnit) as ReturnResult<T>;
         }
 
         /// <summary>
@@ -41,7 +41,7 @@ namespace CCommon.Common
         /// <param name="errorRetryConfig">重试策略(单位:秒)</param>
         /// <param name="methed"></param>
         /// <returns></returns>
-        public static ReturnResult Handle(string errorRetryConfig, Func<ReturnResult> methed)
+        public static ReturnResult Handle(string errorRetryConfig, Func<ReturnResult> methed, TimeUnit timeUnit = TimeUnit.Seconds)
         {
             if (string.IsNullOrEmpty(errorRetryConfig))
             {
@@ -51,23 +51,26 @@ namespace CCommon.Common
             string[] errTime = errorRetryConfig.Split(',');
 
             int errNum = 0;
-            ReturnResult isTrue;
+            ReturnResult returnResult;
             do
             {
                 try
                 {
-                    isTrue = methed();
-                    errNum++;
+                    returnResult = methed();
                 }
                 catch (Exception ex)
                 {
-                    isTrue = ReturnResult.FailResult(ex.ToString(),ex);
-                    System.Threading.Thread.Sleep(int.Parse(errTime[errNum]) * 1000);
+                    returnResult = ReturnResult.FailResult(ex.ToString(),ex);
+                }
+
+                if (!returnResult.IsValid)
+                {
+                    System.Threading.Thread.Sleep((int)timeUnit.toMicroseconds(long.Parse(errTime[errNum])));
                     errNum++;
                 }
-            } while (!isTrue.IsValid && errNum < errTime.Length);
+            } while (!returnResult.IsValid && errNum < errTime.Length);
 
-            return isTrue;
+            return returnResult;
         }
     }
 }
